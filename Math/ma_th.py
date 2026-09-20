2f79af04215faa9baa72bc30bc807c138377f97e
RASESH AGARWAL <raseshcs07@gmail.com>
2026-09-20 17:45:36 +0530

    MATH

---
+++ Math/ma_th.py
@@ -0,0 +1,40 @@
+# MATHS
+# Built-in Math Functions
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+print(x)
+print(y)
+
+x = abs(-7.25)
+print(x)
+
+x = pow(4, 3)
+print(x)
+
+# import math
+import math
+x = math.sqrt(64)
+print(x)
+
+#Import math library
+import math
+
+#Round a number upward to its nearest integer
+x = math.ceil(1.4)                           #Round a number upward to its nearest integer
+y = math.floor(1.4)                          #Round a number downward to its nearest integer
+print(x)
+print(y)
+
+import math
+x = math.pi
+print(x)
+
+
+# Print the lowest value
+print(min(5, 10))
+# Print the highest value
+print(max(5, 10))
+# Print the absolute value
+print(abs(-7.25))
+# Print 4 to the power of 3
+print(pow(4, 3))
