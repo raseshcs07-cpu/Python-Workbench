bcdcb1548e00da2cc54a1bdf436b46b5ed5d74c0
RASESH AGARWAL <raseshcs07@gmail.com>
2026-09-21 19:40:46 +0530

    NONE

--- None/none.py
+++ None/none.py
@@ -1,1 +1,34 @@
 # None
+# None is a special constant in Python that represents the absence of a value.
+x = None
+print(x)
+
+x = None
+print(type(x))
+
+# operator is or is not
+result = None
+if result is None:
+  print("No result yet")
+else:
+  print("Result is ready")
+
+result = None
+if result is not None:
+  print("Result is ready")
+else:
+  print("No result yet")
+
+print(bool(None))
+
+def myfunc():
+ x = 5
+x = myfunc()
+print(x)
+
+
+# Assign None to x
+x = None
+# Check if x is None
+if x is None:
+  print("x is None")
